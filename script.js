9ab2a09a472018b3d45690e0f13d6962f17577ad
jcsodenkamp <97320287+jcsodenkamp@users.noreply.github.com>
2022-09-16 17:00:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const codeCrypt = (str) => {
     str = document.getElementById("crypt").value
-    str = str.toLowerCase()
+
     let newStr = []
     for (let letter in str) {
-        if (str.charCodeAt(letter) === 97) {
-            newStr.push(120)
-        } else if (str.charCodeAt(letter) === 98) {
-            newStr.push(121)
-        } else if (str.charCodeAt(letter) === 99) {
-            newStr.push(122)
-        } else if (str.charAt(letter).match(/[^a-z]/)) {
-            newStr.push(str.charCodeAt(letter))
-        } else {
-            newStr.push(str.charCodeAt(letter) - 3)
-        }
+        newStr.push(str.charCodeAt(letter) -10)
     }
     let array = []
     for (let element of newStr)
@@ -30,17 +20,7 @@ const codeDecrypt = (str) => {
     str = str.toLowerCase()
     let newStr = []
     for (let letter in str) {
-        if (str.charCodeAt(letter) === 120) {
-            newStr.push(97)
-        } else if (str.charCodeAt(letter) === 121) {
-            newStr.push(98)
-        } else if (str.charCodeAt(letter) === 122) {
-            newStr.push(99)
-        } else if (str.charAt(letter).match(/[^a-z]/)) {
-            newStr.push(str.charCodeAt(letter))
-        } else {
-            newStr.push(str.charCodeAt(letter) + 3)
-        }
+        newStr.push(str.charCodeAt(letter) + 10)
     }
 
     let array = []
